a01a7423c638ab37d6d858028e031598940c23d6
Navta23 <guptanavta@gmail.com>
2026-04-13 14:18:15 +0530

    added

---
+++ April-11/TangyAzureFunc/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TangyAzureFunc.Data;
+
+var builder = FunctionsApplication.CreateBuilder(args); 
+
+builder.ConfigureFunctionsWebApplication();
+string connectionString = Environment.GetEnvironmentVariable("AzureSqlDatabase");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Build().Run();
